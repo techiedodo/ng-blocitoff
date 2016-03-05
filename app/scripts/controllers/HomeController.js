0afaa5611427f2a1dba7ca151490001d3f52d74a
Luis Gaytan <gaytan_luis@msn.com>
2016-03-04 23:30:00 -0800

    wip; need to move logic for days left to controller

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -6,13 +6,19 @@
         // create a synchronized array
         $scope.tasks = $firebaseArray(tasksRef);
         
+        $scope.start = Date.now();
+        
         // add new items to the array
         $scope.addTask = function() {
             $scope.tasks.$add({
                 text: $scope.newTaskText,
-                date: $scope.newTaskDate.getTime()
+                created: Firebase.ServerValue.TIMESTAMP,
+                date: $scope.newTaskDate.getTime(),
+                status: "active"
             });
         };
+        
+        
     }
     angular
         .module('blocitoff')
