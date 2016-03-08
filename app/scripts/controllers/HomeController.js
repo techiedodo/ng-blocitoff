5ce486e2e4fb6ea0de6b3bed2d31616fcd10cb79
Luis Gaytan <gaytan_luis@msn.com>
2016-03-07 22:38:27 -0800

    completed checkbox added, expired after 7 days, showing time remaining

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -5,20 +5,32 @@
         
         // create a synchronized array
         $scope.tasks = $firebaseArray(tasksRef);
-        
-        $scope.start = Date.now();
-        
+                
         // add new items to the array
         $scope.addTask = function() {
             $scope.tasks.$add({
                 text: $scope.newTaskText,
                 created: Firebase.ServerValue.TIMESTAMP,
                 date: $scope.newTaskDate.getTime(),
-                status: "active"
+                status: 'active',
+                expired: false
             });
+        };  
+        $scope.elapsedTime = function(currentTask) {
+            var daysLeft = (7 - (Date.now() - currentTask.created)/86400000).toPrecision(2);
+            if (daysLeft > 0) {
+                return daysLeft;
+            } else {
+                tasksRef.child(currentTask.$id).update({status: 'expired'});
+                tasksRef.child(currentTask.$id).update({expired: true});
+                return 0
+            };
         };
-        
-        
+        $scope.completeTask = function(task) {
+            if (task.status) {
+                tasksRef.child(task.$id).update({status: 'complete'});
+            };
+        };
     }
     angular
         .module('blocitoff')
